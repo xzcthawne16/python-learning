06943cc643c461f98b458d1ef80f4d6f1804b3d0
xzcthawne16 <1119128217@qq.com>
2026-06-08 22:37:06 +0800

    取消注释，输出Hello World和Hello Python

--- 01.入门程序.黑马程序员.py
+++ 01.入门程序.黑马程序员.py
@@ -1,5 +1,4 @@
 # 需求:控制台中输出Hello World, Hello Python
-# print("Hello World")
-# print("Hello Python")
-# print("------------------")
-#
+print("Hello World")
+print("Hello Python")
+print("------------------")
