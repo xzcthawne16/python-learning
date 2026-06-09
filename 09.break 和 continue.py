e6f57ab9725292aa841c70f18b8616f28e5d0361
xzcthawne16 <95259306+xzcthawne16@users.noreply.github.com>
2026-06-09 18:08:08 +0800

    Delete 09.break 和 continue.py

--- 09.break 和 continue.py
+++
@@ -1,6 +0,0 @@
-# break:出现在循环中,跳出循环时,while后面的else中的代码不会执行
-# continue:出现在循环中,表示中断本次循环,直接进入下一次
-# while Ture: 指的是不知道循环多少次,一直循环下去
-#生成随机数的方法
-import random
-random_number= random.randint(1,100)#相当于从1-100中生成一个随机整型
