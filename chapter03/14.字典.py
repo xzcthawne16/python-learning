6571697453e2ecc7b84a97e435bf7e6d26c61fd1
xzcthawne16 <95259306+xzcthawne16@users.noreply.github.com>
2026-06-10 15:32:33 +0800

    Update 14.字典.py

--- chapter03/14.字典.py
+++ chapter03/14.字典.py
@@ -1,15 +1,15 @@
-# #字段 -- key不能重复(如果重复,后面的值,会覆盖前面的值) key必须是不可变类型(str,int,float,tuple)
-# dict1={"a":100,"b":99,"c":98,"d":97}
-# print(dict1)
-# print(type(dict1))
-#
-# dict2={0:650,1.5:222,(1,2):854,("a","b"):111}
-# print(dict2)
-#
-# #访问
-# print(dict1["b"])
-# dict1["b"]=101
-# print(dict1)
+#字段 -- key不能重复(如果重复,后面的值,会覆盖前面的值) key必须是不可变类型(str,int,float,tuple)
+dict1={"a":100,"b":99,"c":98,"d":97}
+print(dict1)
+print(type(dict1))
+
+dict2={0:650,1.5:222,(1,2):854,("a","b"):111}
+print(dict2)
+
+#访问
+print(dict1["b"])
+dict1["b"]=101
+print(dict1)
 
 #字典的常用操作
 dict1={"王林":670,"李慕婉":608,"许立国":580}
