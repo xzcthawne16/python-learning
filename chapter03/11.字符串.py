605fcf0da1c6cb51d3624ab1cfa1bec18b02a894
xzcthawne16 <95259306+xzcthawne16@users.noreply.github.com>
2026-06-10 15:31:43 +0800

    Uncomment string methods examples in Python

--- chapter03/11.字符串.py
+++ chapter03/11.字符串.py
@@ -1,27 +1,27 @@
 #字符串的常见方法
-# s="    Hello-Python-Hello-World     "
-#
-# #find() 查找制定字符串中第一次出现的索引位置
-# index=s.find("-")
-# print(index)
-# #count() 统计子字符串在指定字符串中出现的次数
-# num=s.count("o")
-# print(num)
-# #upper() 转为大写
-# s1=s.upper()
-# print(s1)
-# #lower()转为小写
-# s2=s.lower()
-# print(s2)
-# #split()将字符串按照指定字符串切割-列表
-# s3=s.split("-")
-# print(s3)
-# #strip()去除字符串两端的空格
-# s4=s.strip()
-# print(s4)
-#
-# print("------------")
-# print(s) #对于字符串的常见操作,不会改变原来的字符串
+s="    Hello-Python-Hello-World     "
+
+#find() 查找制定字符串中第一次出现的索引位置
+index=s.find("-")
+print(index)
+#count() 统计子字符串在指定字符串中出现的次数
+num=s.count("o")
+print(num)
+#upper() 转为大写
+s1=s.upper()
+print(s1)
+#lower()转为小写
+s2=s.lower()
+print(s2)
+#split()将字符串按照指定字符串切割-列表
+s3=s.split("-")
+print(s3)
+#strip()去除字符串两端的空格
+s4=s.strip()
+print(s4)
+
+print("------------")
+print(s) #对于字符串的常见操作,不会改变原来的字符串
 
 
 
